122d4fe50611a229f63c9bf19f3c854e17bd3a5e
JesuAcevedo <160240903+JesuAcevedo@users.noreply.github.com>
2026-02-18 14:17:01 -0500

    Agrego funciones tradicionales y de flecha a la clase Person

--- main.dart
+++ main.dart
@@ -1,14 +1,24 @@
 void main() {
-  Person emp = new Person(name: 'Jesús', power: 'Acevedo', isAlive: true);
+  Person emp = Person(name: 'Jesús', power: 'Acevedo', isAlive: true);
 
   Person emp2 = Person.fromJson({
     'name': 'YYY',
     'power': 'ZZZ',
     'isAlive': false,
   });
+
   print(emp);
-  //print(emp.nombre);
-  //print(emp.edad);
+  print(emp2);
+
+  
+  emp.saludar();
+  emp.cambiarPoder('Programador');
+
+  
+  print(emp.obtenerNombre());
+  emp.revivir();
+
+  print(emp);
 }
 
 class Person {
@@ -16,12 +26,30 @@ class Person {
   String power;
   bool isAlive;
 
+  
   Person({required this.name, required this.power, required this.isAlive});
 
+  
   Person.fromJson(Map<String, dynamic> json)
-    : name = json['name'] ?? 'No name',
-      power = json['power'] ?? 'No Power',
-      isAlive = json['isAlive'] ?? false;
+      : name = json['name'] ?? 'No name',
+        power = json['power'] ?? 'No Power',
+        isAlive = json['isAlive'] ?? false;
+
+  
+  void saludar() {
+    print('Hola, soy $name');
+  }
+
+ 
+  void cambiarPoder(String nuevoPoder) {
+    power = nuevoPoder;
+  }
+
+  
+  String obtenerNombre() => name;
+
+ 
+  void revivir() => isAlive = true;
 
   @override
   String toString() {
