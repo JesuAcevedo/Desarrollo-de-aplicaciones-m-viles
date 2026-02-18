891f31008aa51437a4d7b56d05a1f9d86e04d70a
JesuAcevedo <160240903+JesuAcevedo@users.noreply.github.com>
2026-02-18 14:27:27 -0500

    Update tarea.dart

--- tareaApi/tarea.dart
+++ tareaApi/tarea.dart
@@ -1,45 +1,58 @@
 void main() {
-
-  Person emp = Person(name:'Jesus', power:'Acevedo', isAlive:true);
+  Person emp = Person(name: 'Jesús', power: 'Acevedo', isAlive: true);
 
   Person emp2 = Person.fromJson({
-    'name' : 'YYY',
-    'power' : 'ZZZ',
-    'isAlive': false
+    'name': 'YYY',
+    'power': 'ZZZ',
+    'isAlive': false,
   });
 
   print(emp);
   print(emp2);
 
-  print(saludar());
-  print(suma(10,20));
+  
+  emp.saludar();
+  emp.cambiarPoder('Programador');
+
+  
+  print(emp.obtenerNombre());
+  emp.revivir();
+
+  print(emp);
 }
 
-
 class Person {
-
   String name;
   String power;
   bool isAlive;
 
+  
   Person({required this.name, required this.power, required this.isAlive});
 
-  Person.fromJson(Map<String,dynamic> json)
+  
+  Person.fromJson(Map<String, dynamic> json)
       : name = json['name'] ?? 'No name',
         power = json['power'] ?? 'No Power',
         isAlive = json['isAlive'] ?? false;
+
+  
+  void saludar() {
+    print('Hola, soy $name');
+  }
+
+ 
+  void cambiarPoder(String nuevoPoder) {
+    power = nuevoPoder;
+  }
+
+  
+  String obtenerNombre() => name;
+
+ 
+  void revivir() => isAlive = true;
 
   @override
   String toString() {
     return 'Person(name: $name, power: $power, isAlive: $isAlive)';
   }
-  
 }
-String saludar() {
-  return 'Hola mundo';
-}
-
-int suma(int a, int b) {
-  return a + b;
-}
-
